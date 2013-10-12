6a728d09f8aba0b313e8fc66abc8efd50a3f32a3
James Pickard <james.pickard+github@gmail.com>
2013-10-12 14:05:48 +0100

    fix detection of erroneous reading. Only insert once every 30s

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var temp = require('node-rasp2c-temp/temp'),
 	db = require('./db.js');
 
 // Log interval in milliseconds.
-var logIntervalMilliseconds = 5 * 1000;
+var logIntervalMilliseconds = 30 * 1000;
 
 var app = express();
 app.configure(function () {
@@ -20,6 +20,9 @@ app.configure(function () {
 	app.use(express.static('static'));
 });
 
+// Record the previous reading, to detect wild fluctuations, which are probably errors.
+var lastGoodTemp = null;
+
 // Repeatedly call the logging function.
 setInterval(function logTemperature() {
 	"use strict";
@@ -31,6 +34,20 @@ setInterval(function logTemperature() {
 		console.error('Even the last good temp was bad! Not inserting anything.');
 		return;
 	}
+
+	// If reading is very different from the last "good" temperature from the sensor, then ignore it.
+	// TODO: Implement this properly:
+	//	* A very different reading from the last one should be ignored.
+	//	* Make the range configurable.
+	//	* If the temperature ever jumps by more than +10 or -10 in one reading,
+	//	  this version will break because all subsequent readings will be
+	//	  discarded.
+	if (lastGoodTemp !== null && (celsius > lastGoodTemp + 15 || celsius < lastGoodTemp - 15)) {
+		console.error('Probably a weird reading (' + celsius + 'C) - very different from the last good one (' + lastGoodTemp + 'C), ignoring.');
+		return;
+	}
+
+	lastGoodTemp = celsius;
 
 	var record = {
 		unix_time: Date.now(),
