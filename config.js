d66471e1f3ce7d74b43b839b1c968666b51f90fe
James Pickard <james.pickard+github@gmail.com>
2017-02-15 13:57:17 +0000

    Copy bits from express-boilerplate

---
+++ config.js
@@ -0,0 +1,20 @@
+'use strict';
+const fs = require('fs-sync');
+
+if (fs.exists('.env') === false) {
+  /* eslint no-console: false */
+  console.log('> cp .env.example .env');
+  fs.copy('.env.example', '.env');
+}
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  app: {
+    port: process.env.APP_PORT
+  },
+  log: {
+    level: process.env.LOG_LEVEL
+  }
+}
